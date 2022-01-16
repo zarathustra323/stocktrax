0af419cc454191441641aa2d0784d78cda294efd
Jacob Bare <jacob@parameter1.com>
2022-01-15 22:03:34 -0600

    Add ref data sync for symbols and exchanges

--- cli/src/index.js
+++ cli/src/index.js
@@ -1,5 +1,7 @@
 import inquirer from 'inquirer';
-import { immediatelyThrow } from '@stocktrax/utils';
+import { chunkArray, immediatelyThrow } from '@stocktrax/utils';
+import { eachSeries, eachOfSeries } from 'async';
+import iex from './iexcloud.js';
 import mongodb from './mongodb.js';
 import refDataMap from './ref-data-map.js';
 
@@ -35,7 +37,35 @@ const { log } = console;
   await mongodb.connect();
   log('MongoDB connected.');
 
-  log(refDataTypes);
+  const now = new Date();
+
+  await eachSeries(refDataTypes, async (endpoint) => {
+    const ref = refDataMap.get(endpoint);
+    const collection = await mongodb.collection({ dbName: 'iexcloud', name: endpoint });
+    log(`Syncing data for ${ref.name}...`);
+    const data = await iex.request({ endpoint });
+
+    log(`Found ${data.length} records`);
+
+    const chunks = chunkArray(data, 1000);
+
+    await eachOfSeries(chunks, async (chunk, index) => {
+      log(` > Processing chunk ${index + 1} of ${chunks.length}...`);
+      const operations = chunk.map((obj) => {
+        const filter = ref.buildFilter(obj);
+        const update = {
+          $set: {
+            _meta: { lastProcessedAt: now },
+            ...obj,
+          },
+        };
+        return { updateOne: { filter, update, upsert: true } };
+      });
+      await collection.bulkWrite(operations);
+    });
+
+    log(`Data sync complete for ${ref.name}`);
+  });
 
   log('Closing from MongoDB...');
   await mongodb.close();
--- cli/src/ref-data-map.js
+++ cli/src/ref-data-map.js
@@ -1,3 +1,26 @@
 export default new Map([
-  ['ref-data/symbols', { name: 'Symbols' }],
+  ['ref-data/symbols', {
+    name: 'Symbols',
+    buildFilter: (obj) => ({ symbol: obj.symbol }),
+  }],
+  ['ref-data/mutual-funds/symbols', {
+    name: 'Mutual Fund Symbols',
+    buildFilter: (obj) => ({ symbol: obj.symbol }),
+  }],
+  ['ref-data/crypto/symbols', {
+    name: 'Cryptocurrency Symbols',
+    buildFilter: (obj) => ({ symbol: obj.symbol }),
+  }],
+  ['ref-data/otc/symbols', {
+    name: 'OTC Symbols',
+    buildFilter: (obj) => ({ symbol: obj.symbol }),
+  }],
+  ['ref-data/market/us/exchanges', {
+    name: 'U.S. Exchanges',
+    buildFilter: (obj) => ({ mic: obj.mic }),
+  }],
+  ['ref-data/exchanges', {
+    name: 'International Exchanges',
+    buildFilter: (obj) => ({ segment: obj.segment }),
+  }],
 ]);
